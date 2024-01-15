ad91680e028a768849cbf763cce6ee5791ee1570
MsWakesho <wakeshosonja@gmail.com>
2024-01-15 14:05:11 +0300

    Still working on the assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name, email) {
+    const destinationUrl = 'http://localhost:3000/users';
+  
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+const configurationObject = {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+
+    },
+    body:JSON.stringify(formData),
+
+};
+
+return fetch(destinationUrl,configurationObject)
+.then(function(response){
+    return response.json();
+})
+
+.then(data =>{
+    console.log('Sucesss',data);
+})
+
+.catch(error => {
+    console.error("Error:Input Invalid", error.message);
+});
+
+}
